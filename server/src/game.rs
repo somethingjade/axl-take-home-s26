a7369b56027ca349fe60f350d18809ca8feb0914
artizanw <artizanw1@gmail.com>
2026-03-29 18:20:50 -0400

    feat: update bot dataset

--- server/src/game.rs
+++ server/src/game.rs
@@ -26,9 +26,10 @@ pub enum Round {
 
 #[derive(Deserialize, Serialize)]
 struct BotPrompt {
-    personality: Vec<String>,
-    playstyle: Vec<String>,
+    personality: String,
+    playstyle: String,
     character_summary: String,
+    behavioral_directive: String,
 }
 
 #[derive(Deserialize)]
@@ -100,7 +101,7 @@ impl GameState {
             used.insert(idx);
             let bot_prompt = serde_json::to_string(&parsed[idx]).unwrap();
             let system_prompt = format!(
-                "You are a player in a social deduction game. Here are instructions for the game: {}. When answering questions, act as if you have an experience related to the prompt. You will act as follows: {}.",
+                "You are a player in a social deduction game. Here are instructions for the game: {}. When answering questions, always act as if you have an experience related to the prompt. Strictly follow this behavioral directive in all responses: {}.",
                 self.bot_intro(id),
                 bot_prompt
             );
